60cbc8a996e4eeb0a5e57ccbce20283496cd4f51
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-05-22 09:24:37 +0800

    added stream utils

---
+++ io/src/main/java/org/mariotaku/commons/io/StreamUtils.java
@@ -0,0 +1,49 @@
+package org.mariotaku.commons.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamUtils {
+
+    private static PreCopyListener EMPTY_PRE_COPY = new PreCopyListener() {
+        @Override
+        public int onPreCopy(int max) {
+            return max;
+        }
+    };
+    private static CopyListener EMPTY_COPY = new CopyListener() {
+        @Override
+        public boolean onCopied(long len, long total) {
+            return true;
+        }
+    };
+
+    public static long copy(InputStream is, OutputStream os, PreCopyListener preCopyListener,
+                            CopyListener copyListener)
+            throws IOException {
+        byte[] buf = new byte[8192];
+        int len;
+        long total = 0;
+        if (preCopyListener == null) {
+            preCopyListener = EMPTY_PRE_COPY;
+        }
+        if (copyListener == null) {
+            copyListener = EMPTY_COPY;
+        }
+        while ((len = is.read(buf, 0, preCopyListener.onPreCopy(buf.length))) != -1) {
+            os.write(buf, 0, len);
+            total += len;
+            if (!copyListener.onCopied(len, total)) break;
+        }
+        return total;
+    }
+
+    public interface PreCopyListener {
+        int onPreCopy(int max);
+    }
+
+    public interface CopyListener {
+        boolean onCopied(long len, long total);
+    }
+}
